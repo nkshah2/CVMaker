ae85955913e9e8b611ee71baf57f3bc6b4582a2a
nkshah2 <centrino12>
2016-10-15 15:40:37 +0530

    everything should come

--- app/src/main/java/nksystems/cvmaker/activity_settings.java
+++ app/src/main/java/nksystems/cvmaker/activity_settings.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.Toast;
 
 import java.util.Arrays;
@@ -23,6 +24,8 @@ public class activity_settings extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.i("Test","commit test");
 
         dbObject=new DatabaseObject(this);
         theme=dbObject.getConnection();
